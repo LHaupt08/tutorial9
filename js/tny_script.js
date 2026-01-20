f7eb893cd569ced6a28b6de112fe55a783d2c5ed
LHaupt1s <LHaupt1s@capetigers.com>
2026-01-20 10:39:47 -0600

    Page 691

---
+++ js/tny_script.js
@@ -0,0 +1,15 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Lukas Haupt
+   Date:   1/20/26
+
+*/
+/*Display the current date and time */
+document.getElementById("dateNow").innerHTML = "m/d/y<Br />h:m:s";
+
+window.alert("Welcome to Tulsa");
